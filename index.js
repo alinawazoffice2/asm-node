5e1b0d1c946cd280cd50db76c62090f289e45170
Ali Hashe <ali.nawaz@hashehouse.com>
2019-07-31 12:43:53 +0500

    inc from main file & others, stack, inp fix

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 var tokenizer = require('./lib/tokenizer');
 var parser = require('./lib/parser');
+var cmp = require('./lib/compiler');
 var Tokenizer = new tokenizer.Tokenizer();
 
 var args = process.argv.slice(2);
 
 switch (args[0]) {
     case 'compile':
-    Tokenizer.readFileContents('main.a', function(contents){
-        //console.log(contents);
-        let tokens = Tokenizer.tokenize(contents);
-        let Parser = new parser.Parser(tokens);
-        Parser.parse();
-    });
-    break;
+        Tokenizer.readFileContents('main.a', function(contents){
+            //console.log(contents);
+            let tokens = Tokenizer.tokenize(contents);
+            let Parser = new parser.Parser(tokens);
+            Parser.parse();
+        });
+        break;
+    case 'build':
+        Tokenizer.readFileContents('main.a', function(contents){
+            //console.log(contents);
+            let tokens = Tokenizer.tokenize(contents);
+            let Parser = new parser.Parser(tokens);
+            Parser.parse(true); // for build
+        });
+        break;
+    case 'run':
+        Tokenizer.readFileContents('build.f', function(contents){
+            let buildBuff = Buffer.from(contents, 'base64').toString('ascii');
+            buildTree = JSON.parse(buildBuff);
+            let CMP = new cmp.Compiler(buildTree);
+            CMP.compile('main');
+        });
+        break;
 }
